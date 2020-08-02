2e0d610036e925e6b2a50cbbca2a30ccd181c76f
Filip <filip.szkarlat@gmail.com>
2020-08-02 09:29:33 +0200

    Corey 3 tutorial

--- ZlyFachowiec/ZlyFachowiec/settings.py
+++ ZlyFachowiec/ZlyFachowiec/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'zf.apps.ZfConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
